620012c0f8a5d8607ab67e2b56e4e43506781c28
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-06-19 04:30:46 +0600

    image and table scraped

--- Missions_To_Mars/scrape_mars.py
+++ Missions_To_Mars/scrape_mars.py
@@ -9,10 +9,11 @@ def title():
 
     nasa_url = 'https://redplanetscience.com/'
     browser.visit(nasa_url)
-    html = browser.html
-    soup = BeautifulSoup(html, "lxml")
 
-    title = soup.find('div', class_= 'content_title').get_text()
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    title = soup.find('div', class_='content_title').get_text()
 
     browser.quit()
 
@@ -27,16 +28,50 @@ def text():
     html = browser.html
     soup = BeautifulSoup(html, "lxml")
 
-    text = soup.find('div', class_= 'article_teaser_body').get_text()
+    text = soup.find('div', class_='article_teaser_body').get_text()
 
     browser.quit()
 
     return text
 
+def image():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    
+    image_url = 'https://spaceimages-mars.com/'
+    browser.visit(image_url)   
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'lxml')  
+
+    image = soup.find("img", class_="headerimage fade-in")["src"]
+    featured_image_url = image_url + image
+
+    browser.quit()
+
+    return featured_image_url
+
+def table():
+    table_url = 'https://galaxyfacts-mars.com/'
+
+    tables = pd.read_html(table_url)
+
+    df = tables[0]
+    df.columns = ['Description', 'Mars', 'Earth']   
+
+    mars_html = df.to_html(index = False)
+    
+    return mars_html
+
+
+
+
 def scrape():
     mars = {}
     mars["title"] = title()
     mars["text"] = text()
+    mars['image'] = image()
+    mars['table'] = table()
 
     return mars
     
